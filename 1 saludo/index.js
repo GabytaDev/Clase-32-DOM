4ad1268c06e2062b9c59336e5e7eb0acd11864d7
GabytaDev <gabyta.loyola@gmail.com>
2021-07-19 11:30:54 -0300

    ejercicio 1 resuelto

--- 1 saludo/index.js
+++ 1 saludo/index.js
@@ -1,6 +1,15 @@
+// Para hacer interactivas nuestras páginas, queremos incorporar un saludo personalizado 
+// , para ello vamos a crear un programa que al iniciarse pida mediante un prompt ingresar 
+// un nombre, y una vez ingresado muestre dentro de un h1 el siguiente saludo 
+// Hola ${nombre}. Te damos la bienvenida.
 
-const nombre = prompt("ingresar un nombre");
+//  Consideraciones:
 
-const nombre = document.getElementById ("#nombre")
+// el título h1 debe tener un tamaño de 22px y la tipografía de Google Fonts Roboto.
 
-nombre.textContent = `Hola ${nombre}, Te damos la bienvenida`
+
+let nombre = prompt("ingresar un nombre")
+
+const titulo = document.querySelector(".saludo")
+
+titulo.textContent = `Hola ${nombre}. Te damos la bienvenida`
